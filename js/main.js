3f55aa222423246fa46c59ab52d4fdf0f42d835b
nkosyrev <shutnik1986@gmail.com>
2022-10-31 04:36:33 +0900

    add: modal-dialog

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector(".modal");
+const modalToggle = document.querySelectorAll("[data-toggle=modal]");
+const modalClose = document.querySelector(".modal-close");
+console.log(modalToggle);
+modalToggle.forEach(element => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.classList.add("is-open");
+  });
+});
+modalClose.addEventListener("click", (event) => {
+  event.preventDefault();
+  modal.classList.remove("is-open");
+});
