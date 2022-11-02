dcd9e19d95f6f249b1d241d40488574769e76b21
nkosyrev <shutnik1986@gmail.com>
2022-11-03 01:18:01 +0900

    fix: icon effects

--- js/main.js
+++ js/main.js
@@ -16,6 +16,10 @@ modalClose.addEventListener("click", (event) => {
 });
 
 
+document.querySelector(".model-icon-link").addEventListener("click", (e)=>{
+  e.target.classList.toggle("icon-active");
+});
+
 /* Создаем префикс +7, даже если вводят 8 или 9 */
 const prefixNumber = (str) => {
   /* если вводят семерку, добавляем ей скобку */
